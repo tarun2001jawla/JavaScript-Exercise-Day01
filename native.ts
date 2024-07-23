69907a1851a6691db397a8bf8fd3f7edddf019a1
Tarun Jawla <tarunjawla2@gmail.com>
2024-07-23 16:05:23 +0530

    updated

--- native.ts
+++ native.ts
@@ -14,7 +14,7 @@ const tMembers: TMember[] = [
     { name: 'Yogesh Khatri', age: 51 }
   ];
 //1.Get array of first names of everyone
-const tFirstNameArray: string[] = members.map(member => member.name.split(' ')[0]);
+const tFirstNameArray: string[] = tMembers.map(member => member.name.split(' ')[0]);
 console.log(tFirstNameArray);
 
   // 2. Make everyone's last names in UPPERCASE in given array of objects
@@ -30,6 +30,8 @@ console.log(tFirstNameArray);
   });
   console.log("Members with age between age 41 & 60 are:", tAgedMembers);
   
+  // 5.
+
   //4.  Get Person with maximum age 
   const tPersonWithMaxAge: TMember = tMembers.reduce((max, curr) => {
     return (curr.age !== undefined && (max.age === undefined || curr.age > max.age)) ? curr : max;
@@ -53,6 +55,28 @@ console.log(tFirstNameArray);
       (result.old = result.old || []).push(member);
     }
     return result;
-  }, {});
+  }, {} as TGroupedMembers);
   console.log("The divided group is:\n", tGroupedMembers);
   
+//9
+const tIndex : number =2;
+const newTMember: TMember = {name:'Raj Singh', age:36};
+tMembers.splice(tIndex, 0, newTMember);
+console.log(tMembers);
+
+//10. Extract properties of object using destructuring
+// const {name, age} = tMembers[0];
+// console.log(name,age); 
+
+  
+  // 11. Rename extracted property of object while destructuring
+  const {name:tFirstname1, age:tAge1} = tMembers[0];
+  console.log(tFirstname1, tAge1);
+  
+    // 12. Destructure any property of an object and use spread operator to get remaining properties in an object
+    const {name:tFirstname2, ...tOtherProperties} = tMembers[0];
+    console.log(tFirstname2, tOtherProperties);
+
+      // 13. Create a new object by copying using spread operator, override one of the properties to assign a new value in the same step
+      const tNewMember: TMember = {...tMembers[2], name:'Rahul'};
+      console.log(tNewMember);
